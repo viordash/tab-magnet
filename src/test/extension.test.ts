fe1951151f6845fee91259361a1feb44e6aae56f
Vitalii Orazov <viordash@mail.ru>
2026-01-21 14:47:30 +0300

    tests: wip. add getPosition test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,14 +1,18 @@
 import * as assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import { getPosition, PAIR_EXTENSIONS, Position } from '../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
+	test('getPosition returns Left for h', () => {		
+		const supportedFile = PAIR_EXTENSIONS['.h'];
+		const currentPath = "~/test_cpp/lib/src/test.h";
+		const tabPath = "~/test_cpp/lib/src/test.cpp";
+		const position = getPosition(supportedFile, currentPath, tabPath);
+		assert.ok(position);
+		assert.equal(position, Position.Left);
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
 	});
